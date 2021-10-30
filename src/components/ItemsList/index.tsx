3c42623e2c0e9d3eb3cf2a33fbcb8b3cc7b77994
Dpartipilo <Dpartipilo@hotmail.com>
2021-10-30 11:07:49 +0100

    Chore: Improved ItemList component styling

--- src/components/ItemsList/index.tsx
+++ src/components/ItemsList/index.tsx
@@ -5,15 +5,20 @@ import styles from './ItemsList.module.scss'
 import { IItems } from "../types"
 import Item from '../Item'
 
-const ItemsList: React.FC<IItems> = ({ items = [] }) => {
+const ItemsList: React.FC<IItems> = ({ items }) => {
 
   return (
-    <div className={styles.ItemsList}>
-      {items.map((item) => {
-        const { nasa_id } = item.data[0]
-        return <Item key={nasa_id} {...item} />
-      })}
-    </div>
+    items.length > 0 ?
+      <div className={styles.ItemsList}>
+        <h2>Results</h2>
+        <div className={styles.itemsContainer}>
+          {items.map((item) => {
+            const { nasa_id } = item.data[0]
+            return <Item key={nasa_id} {...item} />
+          })}
+        </div>
+      </div>
+      : null
   )
 }
 
