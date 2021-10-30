bdd95949ac817fd3078a340dbe0491d7f41136dc
Dpartipilo <Dpartipilo@hotmail.com>
2021-10-30 11:08:39 +0100

    Chore: Improved Search component styling

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
+import React, { FormEvent, useState, useEffect } from 'react'
 import styles from './Search.module.scss';
 
 import Checkbox from '../Checkbox';
@@ -10,40 +10,53 @@ interface ISearch {
 const Search: React.FC<ISearch> = ({ handleSearch }) => {
   const [searchValue, setSearchValue] = useState("");
   const [mediaTypes, setMediaTypes] = useState<string[]>([]);
+  const [searchDisabled, setSearchDisabled] = useState(false);
 
-  const handleOnChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    let selectedMediaTypes = mediaTypes;
+  useEffect(() => {
+    mediaTypes.length > 0 && searchValue.length > 0 ? setSearchDisabled(false) : setSearchDisabled(true);
+  }, [mediaTypes, searchValue])
+
+  const handleOnChange = (evt: any) => {
+    console.log(evt.target.checked)
+    let selectedMediaTypes = [...mediaTypes];
 
     if (evt.target.checked) {
       selectedMediaTypes.push(evt.target.name)
-      setMediaTypes(selectedMediaTypes);
     } else {
       const index = mediaTypes.indexOf(evt.target.name);
       selectedMediaTypes.splice(index, 1);
-      setMediaTypes(selectedMediaTypes);
     };
+
+    setMediaTypes(selectedMediaTypes);
   }
 
   return (
     <div className={styles.Search}>
-      <h1>Nasa search</h1>
 
-      <form onSubmit={(evt) => handleSearch(evt, searchValue, mediaTypes.join(","))}>
-        <input placeholder="Search for..." type="text" value={searchValue}
-          onChange={(evt) => setSearchValue(evt.target.value)}
-        />
+      <div className={styles.formContainer}>
+        <h1>Nasa search</h1>
 
-        <button type="submit">
-          Search
-        </button>
+        <form onSubmit={(evt) => handleSearch(evt, searchValue, mediaTypes.join(","))}>
 
-        <div className={styles.checkboxGroup}>
-          <Checkbox id="checkbox-image" name="image" label="Image" handleOnChange={handleOnChange} />
-          <Checkbox id="checkbox-audio" name="audio" label="Audio" handleOnChange={handleOnChange} />
+          <div className={styles.inputContainer}>
+            <input placeholder="Search for..." type="text" value={searchValue}
+              onChange={(evt) => setSearchValue(evt.target.value)}
+            />
 
-          {/* <Checkbox id="checkbox-video" name="video" label="Video" handleOnChange={handleOnChange} /> */}
-        </div>
-      </form>
+            <button disabled={searchDisabled} type="submit">
+              Search
+            </button>
+          </div>
+
+
+          <div className={styles.checkboxGroup}>
+            <Checkbox id="checkbox-image" name="image" label="Image" handleOnChange={handleOnChange} />
+            <Checkbox id="checkbox-audio" name="audio" label="Audio" handleOnChange={handleOnChange} />
+
+            {/* <Checkbox id="checkbox-video" name="video" label="Video" handleOnChange={handleOnChange} /> */}
+          </div>
+        </form>
+      </div>
 
     </div>
   )
